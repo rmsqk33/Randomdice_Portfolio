60f14927d57f09d08428cc915d7efb9762371b24
rmsqk33 <rkdrbdud123@naver.com>
2023-03-06 20:26:11 +0900

    일부 스크립트 위치 이동

--- UnityProject/Assets/Scripts/UI/Lobby/FLobbyScene.cs
+++ UnityProject/Assets/Scripts/UI/Lobby/FLobbyScene.cs
@@ -7,7 +7,7 @@ public class FLobbyScene : MonoBehaviour
     private void Awake()
     {
 #if DEBUG
-        if(!FServerManager.Instance.IsConnectedServer)
+        if (!FServerManager.Instance.IsConnectedServer)
         {
             FServerManager.Instance.ConnectServer();
             FAccountMananger.Instance.TryLogin();
